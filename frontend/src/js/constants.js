12f15204b62efeed471f936b9178582db7e43b58
Chaos <contact.chaoskjell44@gmail.com>
2025-04-07 17:42:18 +0200

    fixed invite url

--- frontend/src/js/constants.js
+++ frontend/src/js/constants.js
@@ -1,7 +1,7 @@
 export const IMPORT_URL = env.IMPORT_URL || "https://import-api.ticketsbot.cloud"
 export const API_URL = env.API_URL || "http://localhost:8081"
 export const PLACEHOLDER_DOCS_URL = "https://docs.ticketsbot.cloud/setup/placeholders.html"
-export const INVITE_URL = env.INVITE_URL || "https://invite.ticketsbot.cloud"
+export const INVITE_URL = env.INVITE_URL || "https://discord.com/oauth2/authorize?client_id=1331306534193467434&scope=bot+applications.commands&permissions=395942816984"
 
 export const OAUTH = {
     clientId: env.CLIENT_ID || "1325579039888511056",
