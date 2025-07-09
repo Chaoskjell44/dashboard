d8d15d0e4a319c090b6c1e1b4780880bb39650f2
Chaos <real.chaoskjell44@gmail.com>
2025-07-09 15:42:37 +0200

    undo fix

--- frontend/src/includes/Head.svelte
+++ frontend/src/includes/Head.svelte
@@ -1,3 +1,15 @@
 <svelte:head>
+  <title>Hazel a Tickets Cat Dashboard</title>
 
+  <!-- Meta -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Management panel for the Hazel a Tickets Cat Discord bot">
+  
+  <!--link-->
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+  <!-- Icons -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css">
 </svelte:head>
