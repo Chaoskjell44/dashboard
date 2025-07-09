d27215fc2b3fbca6393bf057a9b6e8240ec23915
Chaos <real.chaoskjell44@gmail.com>
2025-07-09 15:02:29 +0200

    fix?

--- frontend/src/includes/Head.svelte
+++ frontend/src/includes/Head.svelte
@@ -1,14 +1,3 @@
 <svelte:head>
- 
 
-  <!-- Meta -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-  <link rel="icon" href="/favicon.ico" type="image/x-icon">
-
-  <!-- Icons -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css">
-  <script src="https://hazelaticketscat1.statuspage.io/embed/script.js"></script>
 </svelte:head>
